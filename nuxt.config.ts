952e39651808d0f6a39342d51027208338cee739
Modenero developers <support@modenero.com>
2024-07-06 17:17:08 -0400

    First commit.

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    /* Application Settings */
+    app: {
+        /* Application Header */
+        head: {
+            title: 'Pythia — Blockchain Insight',
+            meta: [
+                { charset: 'utf-8' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                { name: 'description', content: 'Accountable and verifiable price & data oracles.' },
+            ],
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            ],
+            script: [
+                // { src: '/js/matomo.js' },
+            ],
+        },
+        // baseURL: '/oracles',
+    },
+
+    /* Application Modules */
+    modules: [
+        /* Tailwind CSS */
+        '@nuxtjs/tailwindcss',
+
+        /* Pinia */
+        '@pinia/nuxt',
+
+        /* Internationalization for Nuxt */
+        '@nuxtjs/i18n',
+    ],
+})
