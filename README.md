84d7e529f72de360594b0ce60d4d2465ce72efaf
Modenero developers <support@modenero.com>
2024-09-06 16:08:35 -0400

    Add well-known asset pairs.

--- README.md
+++ README.md
@@ -3,3 +3,91 @@
 ### https://pythia.app.runonflux.io/
 
 ### A Universal (FOSS) Crypto Oracles Service & Data Center
+
+## Well-known Asset Pairs
+> NOTE: There may be up to 65,535 well-known asset pairs.
+
+| Asset ID | Base Currency | Quote Currency |
+|:---|---:|---:|
+| 0x0001 | BTC | USDT |
+| 0x0002 | BCH | USDT |
+| 0x0003 | NEXA | USDT |
+| 0x0004 | TBD | USDT |
+| 0x0005 | TBD | USDT |
+| 0x0006 | TBD | USDT |
+| 0x0007 | TBD | USDT |
+| 0x0008 | TBD | USDT |
+| 0x0009 | TBD | USDT |
+| 0x000a | TBD | USDT |
+| 0x000b | TBD | USDT |
+| 0x000c | TBD | USDT |
+| 0x000d | TBD | USDT |
+| 0x000e | TBD | USDT |
+| 0x000f | TBD | USDT |
+| 0x0010 | BTC | BCH |
+| 0x0011 | BTC | NEXA |
+| 0x0012 | BTC | USDC |
+| 0x0013 | BTC | DAI |
+| 0x0014 | BTC | FDUSD |
+| 0x0015 | BTC | PYUSD |
+| 0x0016 | BTC | USDD |
+| 0x0017 | BTC | TBD |
+| 0x0018 | BTC | TBD |
+| 0x0019 | BTC | TBD |
+| 0x001a | BTC | TBD |
+| 0x001b | BTC | TBD |
+| 0x001c | BTC | TBD |
+| 0x001d | BTC | TBD |
+| 0x001e | BTC | TBD |
+| 0x001f | BTC | TBD |
+| 0x0020 | BCH | BTC |
+| 0x0021 | BCH | NEXA |
+| 0x0022 | BCH | USDC |
+| 0x0023 | BCH | DAI |
+| 0x0024 | BCH | FDUSD |
+| 0x0025 | BCH | PYUSD |
+| 0x0026 | BCH | USDD |
+| 0x0027 | BCH | TBD |
+| 0x0028 | BCH | TBD |
+| 0x0029 | BCH | TBD |
+| 0x002a | BCH | TBD |
+| 0x002b | BCH | TBD |
+| 0x002c | BCH | TBD |
+| 0x002d | BCH | TBD |
+| 0x002e | BCH | TBD |
+| 0x002f | BCH | TBD |
+| 0x0030 | NEXA | BTC |
+| 0x0031 | NEXA | BCH |
+| 0x0032 | NEXA | USDC |
+| 0x0033 | NEXA | DAI |
+| 0x0034 | NEXA | FDUSD |
+| 0x0035 | NEXA | PYUSD |
+| 0x0036 | NEXA | USDD |
+| 0x0037 | NEXA | TBD |
+| 0x0038 | NEXA | TBD |
+| 0x0039 | NEXA | TBD |
+| 0x003a | NEXA | TBD |
+| 0x003b | NEXA | TBD |
+| 0x003c | NEXA | TBD |
+| 0x003d | NEXA | TBD |
+| 0x003e | NEXA | TBD |
+| 0x003f | NEXA | TBD |
+| 0x0040 | USDT | BTC |
+| 0x0041 | USDT | BCH |
+| 0x0042 | USDT | NEXA |
+| 0x0043 | USDT | TBD |
+| 0x0044 | USDT | TBD |
+| 0x0045 | USDT | TBD |
+| 0x0046 | USDT | TBD |
+| 0x0047 | USDT | TBD |
+| 0x0048 | USDT | TBD |
+| 0x0049 | USDT | TBD |
+| 0x004a | USDT | TBD |
+| 0x004b | USDT | TBD |
+| 0x004c | USDT | TBD |
+| 0x004d | USDT | TBD |
+| 0x004e | USDT | TBD |
+| 0x004f | USDT | TBD |
+| 0x0050 | TBD | TBD |
+
+> NOTE: Custom data assertion IDs MUST be 32-bit, starting at `0x00010000`.
--- daemon/server.js
+++ daemon/server.js
@@ -1,1 +1,123 @@
-console.log('\n  Starting Pythia Daemon...\n')
+/* Import modules. */
+import { encodeAddress } from '@nexajs/address'
+import { hash160 } from '@nexajs/crypto'
+import { getCoins } from '@nexajs/purse'
+import {
+    encodeDataPush,
+    encodeNullData,
+    OP,
+} from '@nexajs/script'
+import {
+    binToHex,
+    hexToBin,
+} from '@nexajs/utils'
+import { Wallet } from '@nexajs/wallet'
+
+console.log('\n  Starting Pythia Daemon...')
+
+/* Initialize constants. */
+const PYTHIA_PROTOCOL_ID = 'PYTHIA'
+
+/* Initialize globals. */
+let address
+let wallet
+
+const init = async () => {
+    console.info('  Initializing...\n')
+    console.log('MNEMONIC', process.env.MNEMONIC)
+
+    wallet = await Wallet.init()
+    // console.log('WALLET', wallet)
+
+    address = wallet.address
+    console.log('ADDRESS', address)
+
+    buildPkg()
+}
+
+const buildPkg = async () => {
+    /* Initialize locals. */
+    let coins
+    let contractAddress
+    let lockingScript
+    let nullData
+    let prefix
+    let receivers
+    let response
+    let scriptHash
+    let scriptPubKey
+    let userData
+
+    /* Set prefix. */
+    prefix = 'nexa'
+
+    // NOTE: NexScript v0.1.0 offers a less-than optimized version
+    //       of this (script) contract (w/ the addition of `OP_SWAP`).
+    lockingScript = new Uint8Array([
+        OP.TXOUTPUTCOUNT,
+        OP.THREE,
+        OP.NUMEQUALVERIFY,
+    ])
+    console.info('\n  Script / Contract:', binToHex(lockingScript))
+
+    scriptHash = hash160(lockingScript)
+    console.log('SCRIPT HASH (hex):', binToHex(scriptHash))
+
+    /* Build script public key. */
+    scriptPubKey = new Uint8Array([
+        OP.ZERO, // groupid or empty stack item
+        ...encodeDataPush(scriptHash), // PUSH HASH160(script template)
+        OP.ZERO, // HASH160(args script) or empty stack item
+    ])
+    console.info('\n  Script Public Key:', binToHex(scriptPubKey))
+
+    /* Encode the public key hash into a P2PKH nexa address. */
+    contractAddress = encodeAddress(
+        prefix,
+        'TEMPLATE',
+        scriptPubKey,
+    )
+    console.info('CONTRACT ADDRESS', contractAddress)
+    // nexareg:nqtsq9q2wwv65p4hraae0etlu2w32trv9zn66mcqxz96expj
+
+    coins = await getCoins(process.env.ALICE_WIF, scriptPubKey)
+        .catch(err => console.error(err))
+    console.log('COINS', coins)
+
+    userData = [
+        PYTHIA_PROTOCOL_ID,
+        'PUBLIC_HAHS_GOES/-HERE',
+    ]
+
+    /* Initialize hex data. */
+    nullData = encodeNullData(userData)
+    // console.log('HEX DATA', nullData)
+
+    receivers = [
+        {
+            data: nullData
+        },
+        {
+            address: primaryAddress,
+            satoshis: SATOSHIS,
+        },
+    ]
+
+    // FIXME: FOR DEV PURPOSES ONLY
+    receivers.push({
+        address: contractAddress,
+    })
+    console.log('RECEIVERS', receivers)
+
+    /* Send UTXO request. */
+    response = await buildCoins({
+        coins,
+        receivers,
+        locking: lockingScript,
+        unlocking: false, // NOTE: disables "automatic" transaction signing.
+    })
+    console.log('Send UTXO (response):', response)
+
+}
+
+init()
